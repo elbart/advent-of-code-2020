a37b149c3973703e95a43779e3ab3c2fe1431829
Tim Eggert <tim@elbart.com>
2020-12-07 00:52:58 +0100

    add intersection iterator possiblity

--- src/06/2.rs
+++ src/06/2.rs
@@ -17,9 +17,26 @@ fn compute_results(group_answers: &Vec<HashSet<char>>) -> usize {
         .count()
 }
 
+fn compute_results_intersect(group_answers: &Vec<HashSet<char>>) -> usize {
+    // note: I would have used fold_first() and would have saved the
+    // `init` variable creation, but it's not available in rust-stable
+    let it = group_answers.iter();
+    let init = group_answers
+        .iter()
+        .next()
+        .unwrap_or(&HashSet::new())
+        .clone();
+
+    it.into_iter()
+        .fold(init, |acc, x| acc.intersection(x).cloned().collect())
+        .iter()
+        .count()
+}
+
 fn main() {
     let f = File::open("src/06/input.txt").unwrap();
     let mut final_sum = 0;
+    let mut final_sum_isect = 0;
 
     let mut group_answers = Vec::new();
     for line in BufReader::new(f).lines() {
@@ -31,12 +48,16 @@ fn main() {
         }
 
         final_sum += compute_results(&group_answers);
+        final_sum_isect += compute_results_intersect(&group_answers);
         group_answers.clear();
     }
 
     // collect results from last group, too because we do not reach this
     // computation, since there is no last line...
     final_sum += compute_results(&group_answers);
+    final_sum_isect += compute_results_intersect(&group_answers);
+
+    assert_eq!(final_sum_isect, final_sum);
 
     println!(
         "The sum of numbers of questions to which everyone in each group answered yes: {}",
